f5607c66f94860809b7e494e1beed829aa80f9c1
= <=>
2020-06-18 00:31:54 +0530

    PWA added

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+var CACHE_NAME = 'rajat-cache-v1';
+var urlsToCache = [
+  '/index.html',
+  
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    // Try the cache
+    caches.match(event.request).then(function(response) {
+      // Fall back to network
+      return response || fetch(event.request);
+    })
+  );
+});
